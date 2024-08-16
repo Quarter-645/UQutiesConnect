e17db9f4a266942d032630a26e6ade7732ac4233
Quarter-645 <connor.morrison@uqconnect.edu.au>
2024-08-16 23:43:25 +1000

    added docker files and start up files

--- backend/flask_app/__init__.py
+++ backend/flask_app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask  # type: ignore
-from flask_cors import CORS # type: ignore
-from game_service.extensions.LimiterExtension import limiter
  
 def create_app(): 
     app = Flask(__name__) 
 
     # Register the blueprints 
-    from flask.views.routes import api 
+    from flask_app.views.routes import api 
     app.register_blueprint(api) 
 
     return app
