0d4a5308baa39e09dab61be4e8e6c21066ae6a10
Quarter-645 <connor.morrison@uqconnect.edu.au>
2024-08-18 00:55:32 +1000

    added localhost:3000 to CORS

--- backend/flask_app/__init__.py
+++ backend/flask_app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask  # type: ignore
+from flask_cors import CORS # type: ignore
  
 def create_app(): 
     app = Flask(__name__) 
+    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
 
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite" 
 
