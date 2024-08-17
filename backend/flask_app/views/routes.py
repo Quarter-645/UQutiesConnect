a5291a35e63615d48b1ce44c2fc6ea1e44963d69
arthur osorio <ar1osorio@yahoo.com>
2024-08-17 16:21:46 +1000

    removing password system

--- backend/flask_app/views/routes.py
+++ backend/flask_app/views/routes.py
@@ -1,12 +1,10 @@
 from flask import Blueprint, jsonify, request #type:ignore
 from flask_app.models import db
 from flask_app.models.models import Student, Friendships
-from flask_bcrypt import Bcrypt
 import re
 from datetime import datetime
 
 api = Blueprint('api', __name__)
-bcrypt = Bcrypt(api)
 
 @api.route('/health')
 def health():
@@ -50,25 +48,27 @@ def addFriend():
 @api.route('/createaccount', methods=['POST'])
 def createaccount():
     #regex email checker if not from UQ return 400
-    pattern = r'\b[A-Za-z0-9._%+-]+@uq\.edu\.au\b'
-    email = request.json.get('email')
-    if not (re.fullmatch(pattern, email)):
-        return jsonify({'error': 'Invalid email address - not from UQ'}), 400
+    # pattern = r'\b[A-Za-z0-9._%+-]+@uq\.edu\.au\b'
+    # email = request.json.get('email')
+    # if not (re.fullmatch(pattern, email)):
+    #     return jsonify({'error': 'Invalid email address - not from UQ'}), 400
 
-    newAccount = Student( 
-        username = request.json.get('username'),
-        email = email,
-        password = request.json.get('password'),
-        name=request.json.get('name'), 
-        degree=request.json.get('degree'),
-        dateStarted=request.json.get('dateStarted')
-    )
+    # newAccount = Student( 
+    #     username = request.json.get('username'),
+    #     email = email,
+    #     password = request.json.get('password'),
+    #     name=request.json.get('name'), 
+    #     degree=request.json.get('degree'),
+    #     dateStarted=request.json.get('dateStarted')
+    # )
 
-    # Adds a new record to the database or will update an existing record 
-    db.session.add(Student) 
-    # Commits the changes to the database, this must be called for the changes to be saved 
-    db.session.commit() 
-    return jsonify(Student.to_dict(), {'status': 'ok'}), 201
+    # # Adds a new record to the database or will update an existing record 
+    # db.session.add(Student) 
+    # # Commits the changes to the database, this must be called for the changes to be saved 
+    # db.session.commit() 
+    # return jsonify(Student.to_dict(), {'status': 'ok'}), 201
+    return jsonify("ok"), 201
+
 
 @api.route('/login', methods=['POST'])
 def login():
@@ -76,14 +76,12 @@ def login():
     username = request.json.get('username')
     password = request.json.get('password')
 
-    student = Student.query.filter_by(username = username).first()
-
-    if not student:
-        return jsonify({"error": "Invalid username"}), 401
-    
-    if not bcrypt.check_password_hash(student.password, password):
-        return jsonify({"error": "Invalid password"}), 401
-
-    return jsonify({"message": "Login successful", "username": student.username}), 200
+    existing_Student = (
+        db.session.query(Student)
+        .filter(
+            (Student.username == username) & (Student.password == password))
+        .all()
+    )
+    return jsonify({"message": "Login successful", "username": Student.username}), 200
 
 
