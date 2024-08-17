c52aab21e5364987b36529231c06017598168496
arthur osorio <ar1osorio@yahoo.com>
2024-08-17 15:01:46 +1000

    added create account interaction with DB

--- backend/flask_app/views/routes.py
+++ backend/flask_app/views/routes.py
@@ -33,11 +33,21 @@ def createaccount():
 
     newAccount = Student( 
         username = request.json.get('username'),
-        email = request.json.get('email'),
+        email = email,
         password = request.json.get('password'),
         name=request.json.get('name'), 
         degree=request.json.get('degree'),
         dateStarted=request.json.get('dateStarted')
     )
-    return newAccount
+
+    # Adds a new record to the database or will update an existing record 
+    db.session.add(Student) 
+    # Commits the changes to the database, this must be called for the changes to be saved 
+    db.session.commit() 
+    return jsonify(Student.to_dict(), {'status': 'ok'}), 201
+
+@api.route('/login', methods=['POST'])
+def login():
+    getUsername = request.json
+
 
