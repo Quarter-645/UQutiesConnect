8691d1f56e3968c41d062a17e9984177d841e811
Nora <norakristine.laland@gmail.com>
2024-08-17 12:33:35 +1000

    Created testTheme.js in testTheme folder and tested on a box in homepage file Home.js

--- userinterface/src/pages/Home.js
+++ userinterface/src/pages/Home.js
@@ -1,7 +1,34 @@
 import React from "react";
 
+//
+import { Box, ThemeProvider } from '@mui/material'
+import { testTheme } from '../testTheme/testTheme.js'
+
+export const MuiTest = () => {
+  return (
+    <Box sx={{ 
+      height: '300px',
+      width: {
+        xs: 100,
+        sm: 200,
+        md: 300,
+        lg: 400,
+        xl: 500,
+      },
+      bgcolor: 'secondary.main',
+    }}></Box>
+  )
+}
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <ThemeProvider theme={testTheme}>
+      <MuiTest>
+  <div className='App'>Home</div>;
+  </MuiTest>
+  </ThemeProvider>
+  )
 };
 
 export default Home;
+
