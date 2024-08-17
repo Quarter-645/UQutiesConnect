0911f8a15ab828284d80ffe11942898abcb19414
abhat0 <aadyab010@gmail.com>
2024-08-18 02:54:58 +1000

    can search for friends

--- userinterface/src/TempData/UserData.js
+++ userinterface/src/TempData/UserData.js
@@ -1,21 +1,24 @@
 import { UQuties } from "../assets";
 
-const userProfile = [{
-  username: "not_holly",
-  name: "Holly",
-  lastName: "",
-  profilePicture: UQuties, // Path to the profile picture
-},
-{username: "Alice123",
-  name: "Alice",
-  lastName: "Fletcher",
-  profilePictures: UQuties, 
-}, 
-{username: "mada_04",
-  name: "Adam",
-  lastName: "Kovacevic",
-  profilePicture: UQuties,
-} 
+const userProfile = [
+  {
+    username: "not_holly",
+    name: "Holly",
+    lastName: "",
+    profilePicture: UQuties,
+  },
+  {
+    username: "Alice123",
+    name: "Alice",
+    lastName: "Fletcher",
+    profilePictures: UQuties,
+  },
+  {
+    username: "mada_04",
+    name: "Adam",
+    lastName: "Kovacevic",
+    profilePicture: UQuties,
+  },
 ];
 
 export default userProfile;
--- userinterface/src/pages/AddFriend.js
+++ userinterface/src/pages/AddFriend.js
@@ -1,18 +1,16 @@
-import UserData from "../TempData/UserData";
-
-import Logo from "../assets/UQutieLogo.png";
-
-// import Autocomplete from '@mui/material/Autocomplete';
-
-
 import React, { useState } from "react";
-import {Avatar, ListItemText, List, ListItem, IconButton, 
-  TextField, Grid} from "@mui/material";
+import {
+  Avatar,
+  ListItemText,
+  List,
+  ListItem,
+  IconButton,
+  TextField,
+  Grid,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import DeleteIcon from "@mui/icons-material/Delete";
-import usernameArray from "../TempData/UserData";
-
-
+import userProfile from "../TempData/UserData";
+import { Logo } from "../components";
 
 const Search = () => {
   const [usernames, setUsername] = useState([]);
@@ -20,30 +18,26 @@ const Search = () => {
 
   const handleFriendSearch = (event) => {
     setUsernameInput(event.target.value);
-  }
+  };
 
   const addFriend = () => {
     const trimmedUsername = usernameInput.trim();
-    
-    if(
+
+    if (usernames.includes(trimmedUsername)) {
+      alert("Friend is already added.");
+    } else if (
       trimmedUsername &&
-      usernameArray.includes(trimmedUsername) &&
-      !usernames.includes(trimmedUsername)
+      userProfile.some((user) => user.username === trimmedUsername)
     ) {
       setUsername((prevUsernames) => [...prevUsernames, trimmedUsername]);
       setUsernameInput("");
     } else {
-      alert("Username does not exist or is friend is already added.")
+      alert("Username does not exist.");
     }
-  }
-  
-  const deleteUsername = (usernameToDelete) => {
-    setUsername((prevUsernames) => 
-    prevUsernames.filter((username) => username !== usernameToDelete)
-  );
+    console.log("usernames", usernames);
   };
-    return (
-      <Grid
+  return (
+    <Grid
       container
       direction="column"
       justifyContent="center"
@@ -56,69 +50,48 @@ const Search = () => {
         backgroundPosition: "center",
         minHeight: "100vh",
       }}
-      >
-        <Grid
-        item
-        style={{ textAlign: "center"}}
-        >
-          <h1
+    >
+      <Grid item style={{ textAlign: "center" }}>
+        <Logo />
+        <h1
           style={{
             fontFamily: "Baloo Bhaijaan",
             fontSize: "2.5rem",
             color: "#996FD6",
           }}
-          >
-            ⟡Add a UQutie⟡
-          </h1>
-          <h3 style={{ fontFamily: "Baloo Bhaijaan", color: "#996FD6" }}>
-          Current Friends:
-          </h3>
-          <Grid container spacing={1} alignItems="center">
-            <Grid item>
-              <TextField
+        >
+          ⟡Add a UQutie⟡
+        </h1>
+        <Grid container spacing={1} alignItems="center">
+          <Grid item>
+            <TextField
               id="username-input"
               label="Username..."
               variant="standard"
               value={usernameInput}
               onChange={handleFriendSearch}
-              />
-              {console.log("usernameInput", usernameInput)}
-            </Grid>
-            <Grid item>
-              <IconButton
+            />
+          </Grid>
+          <Grid item>
+            <IconButton
               aria-label="Add Friend"
               onClick={addFriend}
               style={{ fontFamily: "Baloo Bhaijaan", color: "#B399DD" }}
-              >
-                <AddIcon />
-              </IconButton>
-            </Grid>
+            >
+              <AddIcon />
+            </IconButton>
           </Grid>
-          <List>
-            {usernames.map((username, index) => (
-              <ListItem
-              key={index}
-              secondaryAction={
-                <IconButton
-                aria-label="Remove Friend"
-                onClick={() => deleteUsername(username)}
-                style={{ color: "#E3E3E3"}}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              }
-              >
-                <ListItemText primary={username} />
-              </ListItem>
-            ))}
-          </List>
         </Grid>
-
+        <List>
+          {usernames.map((user, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={user} />
+            </ListItem>
+          ))}
+        </List>
       </Grid>
- 
+    </Grid>
+  );
+};
 
-    );
-  };
-  
-  export default Search;
-  
+export default Search;
